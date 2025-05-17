a918c1b180d80dcb3325b2c37c115fa516e575eb
Chokkaku Thanooj <chokkakuthanooj@Chokkakus-MacBook-Air.local>
2025-05-17 11:07:48 +0530

    first commit

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ if (!fs.existsSync(uploadsDir)) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization'
 }));
+
 app.use("/userRoute", userController);
 app.use("/courseRoute", courseController);
 app.use("/cartRoute", cartController);
